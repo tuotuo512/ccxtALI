46ba7267b581ea8ca5f1cff6911f198dc33b0ed7
Administrator <410660480@qq.com>
2023-12-15 14:43:01 +0800

    变更

--- CCXT-AI/runai.py
+++ CCXT-AI/runai.py
@@ -1,49 +1,78 @@
-class QuantitativeTradingAlgorithm:
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-    def __init__(self, data):
-        self.data = data  # 包含市场数据的DataFrame
-        self.position_size = 0  # 当前仓位大小
 
-    def calculate_technical_indicators(self, period):
-        # 计算给定周期的技术指标
-        pass
+""""
+步骤 1: 数据预处理
+首先，您需要对数据进行预处理，包括：
 
-    def decide_trading_signal(self, short_period, long_period):
-        # 基于短周期和长周期的技术指标决定交易信号
-        short_indicator = self.calculate_technical_indicators(short_period)
-        long_indicator = self.calculate_technical_indicators(long_period)
+数据标准化或归一化。
+RSI超买或超卖的标识（通常RSI超过70视为超买，低于30视为超卖）。
+Supertrend信号的编码（正信号为1，负信号为-1）。
+"""
 
-        if short_indicator == 'buy' and long_indicator == 'buy':
-            return 'buy'
-        elif short_indicator == 'sell' and long_indicator == 'sell':
-            return 'sell'
-        else:
-            return 'hold'
+# 步骤 2: 定义网络结构
+# 定义一个简单的神经网络结构。这里是一个基本的例子：
 
-    def adjust_position_size(self, market_sentiment, risk_level):
-        # 根据市场情绪和风险等级调整仓位大小
-        if market_sentiment == 'positive' and risk_level == 'low':
-            self.position_size = 'large'
-        elif market_sentiment == 'negative' or risk_level == 'high':
-            self.position_size = 'small'
-        else:
-            self.position_size = 'medium'
+class TradingNN(nn.Module):
+    def __init__(self):
+        super(TradingNN, self).__init__()
+        self.fc1 = nn.Linear(10, 64)  # 假设有10个输入特征
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 1)  # 输出层，预测仓位大小
 
-    def execute_trade(self, signal):
-        # 执行交易
-        if signal == 'buy':
-            # 执行买入操作
-            pass
-        elif signal == 'sell':
-            # 执行卖出操作
-            pass
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = torch.sigmoid(self.fc3(x))  # 使用sigmoid函数将输出限制在0和1之间
+        return x
 
-    def run_algorithm(self):
-        # 运行交易算法
-        signal = self.decide_trading_signal('5min', '1day')
-        self.adjust_position_size(market_sentiment='positive', risk_level='medium')
-        self.execute_trade(signal)
+# 步骤 3: 数据加载
+# 创建一个 DataLoader 来批量加载数据
 
-# 示例使用
-trading_algo = QuantitativeTradingAlgorithm(market_data)
-trading_algo.run_algorithm()
+from torch.utils.data import DataLoader, TensorDataset
+
+
+# 假设 X_train 是您的特征数据，y_train 是您的目标仓位大小
+train_data = TensorDataset(torch.tensor(X_train).float(), torch.tensor(y_train).float())
+train_loader = DataLoader(dataset=train_data, batch_size=64, shuffle=True)
+
+# 步骤 4: 训练网络
+# 初始化模型、损失函数和优化器，然后开始训练过程。
+
+model = TradingNN()
+criterion = nn.MSELoss()  # 使用均方误差损失
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+# 训练循环
+for epoch in range(num_epochs):
+    for inputs, targets in train_loader:
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, targets.view(-1, 1))
+        loss.backward()
+        optimizer.step()
+
+# 步骤 5: 评估模型
+# 在验证集上评估模型性能。
+# 假设 X_val 是验证集特征，y_val 是验证集目标
+val_data = TensorDataset(torch.tensor(X_val).float(), torch.tensor(y_val).float())
+val_loader = DataLoader(dataset=val_data, batch_size=64, shuffle=False)
+
+# 使用模型进行预测和评估
+model.eval()
+with torch.no_grad():
+    for inputs, targets in val_loader:
+        outputs = model(inputs)
+        # ... 进行评估 ...
+
+# 步骤 6: 实际应用
+# 根据模型输出调整仓位大小，并实施交易策略。
+
+# 实际预测
+model.eval()
+with torch.no_grad():
+    real_time_data = torch.tensor(some_real_time_data).float()
+    position_size = model(real_time_data)
+    # ... 根据position_size执行买卖操作 ...
